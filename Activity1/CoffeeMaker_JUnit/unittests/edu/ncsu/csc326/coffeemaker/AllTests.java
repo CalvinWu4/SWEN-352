cba6ed9cd2e99052c0561e7ec3335f7a363de451
Kaloshade <jsj1027@g.rit.edu>
2017-09-25 16:16:56 -0400

    46 inventory tests

--- Activity1/CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/AllTests.java
+++ Activity1/CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/AllTests.java
@@ -8,9 +8,9 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		//$JUnit-BEGIN$
-		suite.addTestSuite(CoffeeMakerTest.class);
+		//suite.addTestSuite(CoffeeMakerTest.class);
 		suite.addTestSuite(InventoryTest.class);
-		suite.addTestSuite(MainTest.class);
+		//suite.addTestSuite(MainTest.class);
 		suite.addTestSuite(RecipeBookTest.class);
 		suite.addTestSuite(RecipeTest.class);
 		//$JUnit-END$
