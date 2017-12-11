10d0ee66eae133004e982f4acf316f69a841b399
noesterle <noesterle1@gmail.com>
2017-12-11 15:37:37 -0500

    Added files to edit for Mockito part.

---
+++ Activity5/CoffeeMaker_Mockito/src/edu/ncsu/csc326/coffeemakerMockito/exceptions/InventoryException.java
@@ -0,0 +1,11 @@
+package edu.ncsu.csc326.coffeemakerMockito.exceptions;
+
+public class InventoryException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public InventoryException(String msg) {
+		super(msg);
+	}
+
+}
